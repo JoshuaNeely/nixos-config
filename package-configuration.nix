f51c11eed43a067b1fa0c97503a582e9d0abc1a3
Joshua Neely <joshua.a.neely@gmail.com>
2025-05-11 17:29:27 +0200

    initial nixos configuration

---
+++ package-configuration.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, modulesPath, ... }:
+# with pkgs prepends the pkgs prefix to each entry
+# without it you would need "pkgs.package_1" "pkgs.package_2" etc
+{
+	environment.systemPackages = [
+	  pkgs.git
+	  pkgs.wget
+	  pkgs.vim
+	];
+}
