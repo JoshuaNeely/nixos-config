47a98ea184e8371238e8903ea85ee3fe242492e4
Joshua Neely <joshua.a.neely@gmail.com>
2025-05-11 17:48:03 +0200

    set up for using dynamicly linked libraries

--- package-configuration.nix
+++ package-configuration.nix
@@ -1,11 +1,15 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-# with pkgs prepends the pkgs prefix to each entry
-# without it you would need "pkgs.package_1" "pkgs.package_2" etc
 {
     environment.systemPackages = with pkgs; [
         git
         wget
         vim
     ];
+
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+        # add dynamic libraries here
+    ]
 }
+
