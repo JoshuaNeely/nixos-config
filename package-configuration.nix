794e67b49c4cd5dd7b9e6fe72852514765ca348f
Joshua Neely <joshua.a.neely@gmail.com>
2025-05-11 17:35:54 +0200

    refining config

--- package-configuration.nix
+++ package-configuration.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, modulesPath, ... }:
+
 # with pkgs prepends the pkgs prefix to each entry
 # without it you would need "pkgs.package_1" "pkgs.package_2" etc
 {
-	environment.systemPackages = [
-	  pkgs.git
-	  pkgs.wget
-	  pkgs.vim
-	];
+    environment.systemPackages = with pkgs; [
+        git
+        wget
+        vim
+    ];
 }
