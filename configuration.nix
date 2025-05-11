248e38297d71de02857d80ff10e64e7d5bcc5e76
Joshua Neely <joshua.a.neely@gmail.com>
2025-05-11 14:04:27 -0500

    enable ssh server

--- configuration.nix
+++ configuration.nix
@@ -84,13 +84,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
