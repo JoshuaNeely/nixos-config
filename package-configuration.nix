fca01c00135ff16b876716c6baf6e9ab3629d208
Joshua Neely <joshua.a.neely@gmail.com>
2025-05-19 08:31:00 -0500

    add lots of programs

--- package-configuration.nix
+++ package-configuration.nix
@@ -7,10 +7,63 @@
         # dev tools
         git
         wget
+        podman
+        inotify-tools
+        udiskie
+
+        # editors
         vim
+        neovim
+        emacs
+          ripgrep # used by doom emacs
+          fd # used by doom emacs
+
+        # password manager
+        gnupg
+        pass
+        pinentry-curses
+
+        # games
         steamcmd
+        discord
+
+        # browser
         firefox
+
+        # helps find keycodes that hypr config uses (and other tools?)
+        wev
+
+        # app launchers
+        hyprlauncher
+
+        # file manager
+        yazi
+
+        # terminals
+        kitty
+
+        # hyprland stuff
+        waybar # configurable topbar
+        dunst # notifation daemon
+        libnotify # needed for notification daemons
+        swww # wallpaper daemon
+        rofi-wayland # app launcher
+        wl-clipboard # expose wl-copy and wl-paste
+        networkmanagerapplet # adds a widget to waybar(?) that allows choosing network
+        hypridle # handles detecting idleness and forwarding signals, including lock signals
+        hyprlock # lock screen
     ];
+
+    services.pcscd.enable = true;
+    programs.gnupg.agent = {
+       enable = true;
+       enableSSHSupport = true;
+    };
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
 
     programs.nix-ld.enable = true;
     programs.nix-ld.libraries = with pkgs; [
